623671ebb36f2b8e044b0a7a90e7813a2b8fc5f8
Mayank Rikh <mayankrikh@gmail.com>
2019-07-05 11:03:46 +0530

    Updated url

--- db/mongoose.js
+++ db/mongoose.js
@@ -7,7 +7,7 @@ const retryConnect = () => {
     console.log(process.env.MONGOLAB_URI)
     const url = debug ? 'mongodb://127.0.0.1:27017/Memory' : process.env.MONGOLAB_URI
 
-    mongoose.connect(process.env.MONGOLAB_URI, {
+    mongoose.connect(url, {
         useNewUrlParser : true,
         useCreateIndex : true,
         useFindAndModify : false
