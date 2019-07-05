ae35b06e03c19c8c923cc6a7653f37bd1c5209f4
Mayank Rikh <mayankrikh@gmail.com>
2019-07-05 10:16:07 +0530

    retry connect

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/Memory', {
-    useNewUrlParser : true,
-    useCreateIndex : true,
-    useFindAndModify : false
-}, (error) => {
-    if (error){
-        console.log('Connection Error: ' + error)
-    }
-})
+const retryConnect = () => {
+    mongoose.connect('mongodb://127.0.0.1:27017/Memory', {
+        useNewUrlParser : true,
+        useCreateIndex : true,
+        useFindAndModify : false
+    }, (error) => {
+        if (error){
+            console.log('Connection Error: ' + error)
+            setTimeout(retryConnect, 5000)
+        }
+    })
+}
