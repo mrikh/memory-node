fac353364a66083ab8644e8ed84670726f51f060
Mayank Rikh <mayankrikh@gmail.com>
2019-07-05 10:59:28 +0530

    minor

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,9 +4,10 @@ const debug = process.env.port === 3000
 
 const retryConnect = () => {
 
+    console.log(process.env.MONGOLAB_URI)
     const url = debug ? 'mongodb://127.0.0.1:27017/Memory' : process.env.MONGOLAB_URI
 
-    mongoose.connect(url, {
+    mongoose.connect(process.env.MONGOLAB_URI, {
         useNewUrlParser : true,
         useCreateIndex : true,
         useFindAndModify : false
